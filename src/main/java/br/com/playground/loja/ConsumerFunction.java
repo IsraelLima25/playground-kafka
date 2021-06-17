133174add64390536a4e622439855b09ad1f639a
israel <israel_lima_16@hotmail.com>
2021-06-17 15:57:38 -0300

    refatorando codigo e paralelizando consumidores em particoes dos topicos

---
+++ src/main/java/br/com/playground/loja/ConsumerFunction.java
@@ -0,0 +1,8 @@
+package br.com.playground.loja;
+
+import org.apache.kafka.clients.consumer.ConsumerRecord;
+
+public interface ConsumerFunction {
+
+    void consume(ConsumerRecord<String,String> record);
+}
